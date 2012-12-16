291e49063bd3c6a35e7d31cef0065b11163dc477
Matteo Landi <matteo@matteolandi.net>
2012-12-16 12:15:04 +0100

    Add a new days application (was amounts)

---
+++ src/days.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+from itertools import groupby
+
+import web
+
+from config import LATEST_DAYS_DATE_FORMAT
+from expenses import ExpensesInBetween
+from formatters import dateformatter
+from models import Expense
+from utils import applicationinitializer
+from utils import parsedateparams
+from utils import protected
+from utils import jsonify
+from utils import BaseHandler
+
+
+
+urls = (
+    '/days.json', 'DaysHandler',
+)
+
+
+application = web.application(urls, globals())
+applicationinitializer(application)
+
+
+class DayWrapper(object):
+    __serializable__ = {
+            'date': lambda o: dateformatter(o.d[0], LATEST_DAYS_DATE_FORMAT),
+            'updated': lambda o: dateformatter(o.d[1]),
+            'amount': lambda o: o.d[2],
+            'delta': lambda o: int(o.d[3]),
+            'currency': lambda o: o.currency,
+            }
+
+    def __init__(self, day, currency):
+        self.d = day
+        self.currency = currency
+
+
+def AccumulateDayAggregate(today):
+    """
+    Factory of functions to be used to compute per-day aggregation
+    """
+    def inner((date, amount, updated, delta), expense):
+        """
+        Function to be used together with `reduce` to compute aggregate
+        information of a `day` object (i.e. date, amount per day, last modified
+        date and time difference in respect of today).
+
+        The function returns a inner function
+        """
+        return (
+                expense.date,
+                amount + expense.amount if not expense.deleted else amount,
+                expense.updated if updated is None or expense.updated > updated
+                        else updated,
+                (expense.date - today).days)
+    return inner
+
+
+def ComputeDayAggregate(expenses):
+    """
+    Iterate all the expenses and compute, for each day, how much have been
+    spent, when is the last time a new expense has been created for a certain
+    day, and what is the time delta (in days) between the day under process and
+    today.
+    """
+    today = datetime.today()
+    (name, amount, updated, delta) = reduce(
+            AccumulateDayAggregate(today), expenses, (None, 0, None, -99999))
+    return name, updated, amount, delta
+
+
+def PlainDate(expense):
+    """
+    Get the day in which the expense has been created (info regarding time is
+    not taken into consideration).
+    """
+    return dateformatter(expense.date, LATEST_DAYS_DATE_FORMAT)
+
+
+class DaysHandler(BaseHandler):
+    @protected
+    def GET(self):
+        since, to, latest = parsedateparams()
+
+        expenses = (
+                ExpensesInBetween(self.current_user().id, since, to, latest)
+                .order_by(Expense.date.asc())
+                .all())
+
+        days = [ComputeDayAggregate(group)
+                    for (key, group) in groupby(
+                        expenses, key=PlainDate)]
+
+        return jsonify(
+                days=[DayWrapper(d, self.current_user().currency)
+                    for d in days])
+
+
+if __name__ == '__main__':
+    application.run()
