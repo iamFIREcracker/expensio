264db03071cc3923787504102d2885099c882e8d
Matteo Landi <matteo@matteolandi.net>
2012-12-16 13:53:09 +0100

    dateformatter: add support for user specified date format

--- src/formatters.py
+++ src/formatters.py
@@ -7,5 +7,8 @@ from config import DATE_FORMAT
 
 
 
-def dateformatter(value):
-    return datetime.strftime(value, DATE_FORMAT)
+def dateformatter(value, format_=DATE_FORMAT):
+    """
+    Convert the input `datetime` object into a string
+    """
+    return datetime.strftime(value, format_)
