b0bf34d0a907ceec53b49373fa8a35508945e129
Matteo Landi <matteo@matteolandi.net>
2012-12-18 20:15:14 +0100

    Factor out a new `users` module

--- src/main.py
+++ src/main.py
@@ -22,12 +22,10 @@ from expenses import ExpensesAddHandler
 from expenses import ExpensesEditHandler
 from expenses import ExpensesDeleteHandler
 from expenses import ExpensesImportHandler
+from users import UsersEditHandler
 from forms import expenses_add
-from forms import users_edit
 from forms import FORM_DATE_FORMAT
 from utils import applicationinitializer
-from utils import me
-from utils import protected
 from utils import BaseHandler
 
 
@@ -80,27 +78,6 @@ class LogoutHandler():
         web.setcookie('user', '', expires=time.time() - 86400)
         web.seeother('/')
 
-
-
-class UsersEditHandler(BaseHandler):
-    @protected
-    @me
-    def GET(self, id):
-        form = users_edit()
-        user = self.current_user()
-        form.fill(id=user.id, name=user.name, currency=user.currency)
-        return web.ctx.render.users_edit_complete(users_edit=form)
-
-    @protected
-    @me
-    def POST(self, id):
-        form = users_edit()
-        if form.validates():
-            u = self.current_user()
-            u.name = form.d.name
-            u.currency = form.d.currency
-            web.ctx.orm.add(u)
-        return web.ctx.render.users_edit(users_edit=form)
 
 
 
---
+++ src/users.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import web
+
+from forms import users_edit
+from utils import applicationinitializer
+from utils import me
+from utils import protected
+from utils import BaseHandler
+
+
+urls = (
+    '/users/(.+)/edit', 'UsersEditHandler',
+)
+
+application = web.application(urls, globals())
+applicationinitializer(application)
+
+
+
+class UsersEditHandler(BaseHandler):
+    @protected
+    @me
+    def GET(self, id):
+        form = users_edit()
+        user = self.current_user()
+        form.fill(id=user.id, name=user.name, currency=user.currency)
+        return web.ctx.render.users_edit_complete(users_edit=form)
+
+    @protected
+    @me
+    def POST(self, id):
+        form = users_edit()
+        if form.validates():
+            u = self.current_user()
+            u.name = form.d.name
+            u.currency = form.d.currency
+            web.ctx.orm.add(u)
+        return web.ctx.render.users_edit(users_edit=form)
+
+
+if __name__ == '__main__':
+    application.run()
