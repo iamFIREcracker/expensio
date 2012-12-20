7bcea5ac9534b6f947b8ef82af204a3e750b5cf0
Matteo Landi <matteo@matteolandi.net>
2012-12-20 20:02:04 +0100

    Fix build

--- src/expenses.py
+++ src/expenses.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from datetime import datetime
-
 import web
 
 import formatters
@@ -11,7 +9,6 @@ from forms import expenses_add
 from forms import expenses_edit
 from forms import expenses_import
 from forms import fetch_expenses
-from forms import FORM_DATE_FORMAT
 from models import Expense
 from utils import applicationinitializer
 from utils import active
