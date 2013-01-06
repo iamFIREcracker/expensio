f0758a8c4cd6408fbda60a535c17532d453d5d19
Matteo Landi <matteo@matteolandi.net>
2013-01-06 19:35:00 +0100

    Use a single file for fabric

--- fabfile.py
+++ fabfile.py
@@ -4,6 +4,7 @@
 
 import sys
 
+from fabric.api import hide
 from fabric.api import cd
 from fabric.api import env
 from fabric.api import local
@@ -17,8 +18,34 @@ from fabric.colors import red
 from fabric.decorators import task
 
 
+env.repo_url = 'https://iamFIREcracker@bitbucket.org/iamFIREcracker/expenses'
+
+
+def vagrant_key():
+    ''' Get the ssh key to access the virtual box machine. '''
+    with settings(hide('stdout', 'stderr')):
+        result = local('vagrant ssh-config | grep IdentityFile', capture=True)
+    return result.split()[1]
+
+
+@task
+def dev():
+    ''' Use virtual machine settings. '''
+    env.user = 'vagrant'
+    env.hosts = ['127.0.0.1:2222']
+    env.key_filename = vagrant_key()
+
+    env.site_path = '/srv/www/expenses'
+    env.venv_path = '/srv/www/expenses/venv'
+    env.site_url  = 'http://expenses.matteolandi.net:9090'
+
+    env.skip_repo = True
+
+
+
 def _happy():
     print(green('\nLooks good from here!\n'))
+
 
 def _sad():
     print(red(r'''
@@ -79,6 +106,14 @@ def sdo(cmd=""):
 
 
 @task
+def papply():
+    '''Apply Puppet manifest. Usable from other commands or the CLI.'''
+    require('user')
+
+    sdo('FACTER_APPNAME=expenses FACTER_USER=%s puppet apply --modulepath=puppet/modules/ puppet/base.pp' % env.user)
+
+
+@task
 def vcmd(cmd=""):
     '''Run a virtualenv-based command in the site directory.  Usable from other commands or the CLI.'''
     require('site_path')
@@ -91,6 +126,7 @@ def vcmd(cmd=""):
     if cmd:
         with cd(env.site_path):
             run(env.venv_path.rstrip('/') + '/bin/' + cmd)
+
 
 @task
 def vsdo(cmd=""):
@@ -106,6 +142,24 @@ def vsdo(cmd=""):
         with cd(env.site_path):
             sudo(env.venv_path.rstrip('/') + '/bin/' + cmd)
 
+
+@task
+def vcreate():
+    '''Create the virtualenv.  Usable from other commands or the CLI.'''
+    require('venv_path')
+
+    run('mkdir -p %s' % env.venv_path)
+    run('virtualenv %s --no-site-packages --distribute' % env.venv_path)
+
+    vupdate()
+
+
+@task
+def vupdate():
+    '''Update the virtualenv.  Usable from other commands or from the CLI.'''
+    vcmd('python setup.py install')
+
+
 @task
 def check():
     '''Check that the home page of the site returns an HTTP 200.'''
@@ -120,8 +174,26 @@ def check():
     else:
         _happy()
 
+
 @task
-def retag():
+def rclone():
+    ''' Clone the app repository repository. '''
+    require('repo_url')
+    require('site_path')
+
+    run('mkdir -p %s' % env.site_path)
+    run('hg clone %s %s' % (env.repo_url, env.site_path))
+
+
+@task
+def rupdate():
+    ''' Update the repository. '''
+
+    cmd('hg pull -u')
+
+
+@task
+def rtag():
     '''Check which revision the site is at and update the local tag.
 
     Useful if someone else has deployed (which makes your production/staging local
@@ -136,3 +208,40 @@ def retag():
     local('hg tag --local --force %s --rev %s' % (env.env_name, current))
 
 
+
+
+@task
+def bootstrap():
+    ''' Configure the app '''
+    if 'skip_repo' not in env:
+        print(cyan("Cloning repo..."))
+        rclone()
+
+    print(cyan("Applying puppet manifest..."))
+    papply()
+
+    print(cyan('\nCreating venv...'))
+    vcreate()
+
+
+@task
+def update():
+    ''' Update everything related to the app. '''
+    if 'skip_repo' not in env:
+        print(cyan("Cloning repo..."))
+        rupdate()
+
+    print(cyan("Applying puppet manifest..."))
+    papply()
+
+    print(cyan('\nUpdating venv...'))
+    vupdate()
+
+    restart()
+
+
+@task
+def restart():
+    ''' Restart the app.  Usable from other commands or from the CLI.'''
+    print(cyan("Restarting gunicorn..."))
+    cmd("sudo supervisorctl restart expenses")
