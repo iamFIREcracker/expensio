b7d7cc553fbc25c96618ea626a96de556a81f9e8
Matteo Landi <matteo@matteolandi.net>
2013-01-07 22:04:22 +0100

    Move static/ outside the app package

--- app/models.py
+++ app/models.py
@@ -40,6 +40,7 @@ def eurocurrency():
 
 class User(Base):
     __tablename__ = 'user'
+    __table_args__ = {'useexisting': True}
 
     id = Column(String, default=_uuid, primary_key=True)
     created = Column(DateTime, default=datetime.now)
@@ -59,6 +60,7 @@ def expenseid(context):
 
 class Expense(Base):
     __tablename__ = 'expense'
+    __table_args__ = {'useexisting': True}
 
     id = Column(String, default=_uuid, primary_key=True)
     original_id = Column(String, ForeignKey('expense.id'), default=expenseid)
