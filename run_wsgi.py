e10e74d47e642bcfbf8458cd604dfb170e448e72
Matteo Landi <matteo@matteolandi.net>
2013-01-08 20:25:45 +0100

    Use a single script to run the wsgi app.
    the file local_settings is used to use werkzeug.DebuggedApplication

--- run_wsgi.py
+++ run_wsgi.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+
+from werkzeug.debug import DebuggedApplication
 from app import app
 
-app = app.wsgifunc()
+import local_settings
+
+
+if local_settings.DEBUG:
+    def nointernalerror():
+        raise sys.exc_info()
+    app.internalerror = nointernalerror
+
+    app = DebuggedApplication(app.wsgifunc(), evalex=True)
+else:
+    app = app.wsgifunc()
