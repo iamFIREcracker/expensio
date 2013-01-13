048809afd1d9df7d37780078726d66ac60f25348
Matteo Landi <matteo@matteolandi.net>
2013-01-13 12:46:02 +0100

    Disable palette colors: everything is now black'n'white

--- static/js/palette.manager.js
+++ static/js/palette.manager.js
@@ -1,13 +1,14 @@
 var PaletteManager = (function() {
     var chart = '#555555';
     var palette = [
-        {bg: '#FF1919', fg: '#ffe',}, // red
-        {bg: '#FF9C19', fg: '#222',}, // orange
-        {bg: '#FFE819', fg: '#222',}, // yellow
-        {bg: '#10A510', fg: '#ffe',}, // blue
-        {bg: '#3333FF', fg: '#ffe',},
-        {bg: 'indigo',  fg: '#ffe',},
-        {bg: 'violet',  fg: '#ffe',},
+          {bg: '#333',    fg: '#ffe',},
+        //{bg: '#FF1919', fg: '#ffe',}, // red
+        //{bg: '#FF9C19', fg: '#222',}, // orange
+        //{bg: '#FFE819', fg: '#222',}, // yellow
+        //{bg: '#10A510', fg: '#ffe',}, // blue
+        //{bg: '#3333FF', fg: '#ffe',},
+        //{bg: 'indigo',  fg: '#ffe',},
+        //{bg: 'violet',  fg: '#ffe',},
     ];
     var mapping = null;
 
