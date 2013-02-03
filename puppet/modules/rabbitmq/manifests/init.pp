c3235457a76b0a9d0412bb20f640e159508dedb2
Matteo Landi <matteo@matteolandi.net>
2013-02-03 23:04:57 +0100

    Fix startup script for rabbit

--- puppet/modules/rabbitmq/manifests/init.pp
+++ puppet/modules/rabbitmq/manifests/init.pp
@@ -2,7 +2,7 @@ class rabbitmq {
   package { [ 'rabbitmq-server' ]:
     ensure => 'installed',
   }
-  service { 'rabbitmq':
+  service { 'rabbitmq-server':
     ensure => running,
     require => Package['rabbitmq-server']
   }
