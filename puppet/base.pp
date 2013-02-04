3cc7d4f942a5446cb8bfa38e83d506484d17a05c
Matteo Landi <matteo@matteolandi.net>
2013-02-04 22:53:30 +0100

    Globally set the path to execute commands

--- puppet/base.pp
+++ puppet/base.pp
@@ -5,8 +5,11 @@ include python
 include rabbitmq
 include supervisor
 
+Exec {
+  path => [ "/usr/local/sbin", "/usr/local/bin", "/usr/sbin", "/usr/bin", "/sbin", "/bin", ],
+}
+
 exec { "add_${user}_to_group_www-data":
-  path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
   command => "usermod -a -G www-data ${user}",
   unless => "id ${user} | grep www-data",
   require => Package[nginx]
