a646b6872f6e5bf2161b2808fc75e6327d441909
Matteo Landi <matteo@matteolandi.net>
2013-02-08 21:29:12 +0100

    Load the color palette as first operation

--- static/js/palette.manager.js
+++ static/js/palette.manager.js
@@ -1,7 +1,10 @@
 var PaletteManager = (function() {
+    var logger = null;
+    var onReadySuccess = null;
+
     var chart = '#555555';
     var palette = [
-          //{bg: '#333',    fg: '#ffe',},
+        {bg: '#333',    fg: '#ffe',}, // default
         {bg: '#FF1919', fg: '#ffe',}, // red
         {bg: '#FF9C19', fg: '#222',}, // orange
         {bg: '#FFE819', fg: '#222',}, // yellow
@@ -16,22 +19,47 @@ var PaletteManager = (function() {
         mapping = Object();
     };
 
-    return {
-        onReady: function() {
-            init();
-        },
 
-        onMonthChange: function() {
+    var onCategoryListSuccess = function(data) {
+        _.map(data.categories, function(catname) {
+            mapping[catname] = _.size(mapping) + 1; // skip default!
+        });
+        onReadySuccess();
+    };
+
+    var onCategoryListError = function(data) {
+        logger.error('Something went wrong while contacting the server');
+    };
+
+    return {
+        onReady: function(logger_, onReadySuccess_) {
+            logger = logger_;
+            onReadySuccess = onReadySuccess_;
+
             init();
+
+            $.ajax({
+                dataType: 'json',
+                type: 'GET',
+                url: '/categories/names',
+                success: onCategoryListSuccess,
+                error: onCategoryListError,
+            });
         },
 
 
         get: function(key) {
             if (key in mapping)
-                return mapping[key] % palette.length;
+                return 1 + (mapping[key] % (palette.length - 1));
             else {
-                mapping[key] = size(mapping);
-                return mapping[key] % palette.length;
+                var msg = '<h4>Reloading the page!</h4>'
+                        + 'A new category has been added to the system';
+                logger.warn(msg, function() {
+                    setTimeout(function() {
+                        location.reload();
+                    }, 2000);
+                });
+                return 0;
             }
         },
 
