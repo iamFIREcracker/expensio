de42f78c2cc9e150d11b11e59ef8cdd97229e38e
Matteo Landi <matteo@matteolandi.net>
2013-02-24 13:55:50 +0100

    Better sorting logic for category names + Recurrences

--- app/controllers/categories.py
+++ app/controllers/categories.py
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 
 from sqlalchemy.sql import distinct
+from sqlalchemy.sql import func
 
 import web
 
 from app.models import Expense
+from app.models import Recurrence
 from app.utils import protected
 from app.utils import jsonify
 from app.utils import BaseHandler
@@ -16,8 +18,21 @@ class CategoriesNamesHandler(BaseHandler):
 
     @protected
     def GET(self):
-        categories = (web.ctx.orm.query(distinct(Expense.category))
+        categories1 = (
+            web.ctx.orm.query(
+                distinct(Expense.category), func.min(Expense.created))
                 .filter_by(user_id=self.current_user().id)
-                .order_by(Expense.created))
+                .group_by(Expense.category)
+                .order_by(Expense.created)
+                .all())
+        categories2 = (
+            web.ctx.orm.query(
+                distinct(Recurrence.category), func.min(Recurrence.created))
+                .filter_by(user_id=self.current_user().id)
+                .group_by(Recurrence.category)
+                .order_by(Recurrence.created)
+                .all())
+        categories = sorted(categories1 + categories2,
+                            key=lambda c: c[1])
         return jsonify(
                 categories=sorted([c[0] for c in categories]))
