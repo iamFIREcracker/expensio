6857db59c43ceda52adec787dfb4fa7f6268c6ab
Matteo Landi <matteo@matteolandi.net>
2013-03-10 12:58:58 +0100

    Add a task which resets/initialize user categories depending on her expenses

--- app/tasks.py
+++ app/tasks.py
@@ -2,13 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import os
+from collections import Counter
 
+import sqlalchemy
 from PIL import Image
 
+import app.config as config
 import app.formatters as formatters
 from app.celery import celery
 from app.database import db_session
+from app.models import Category
 from app.models import Expense
+from app.models import User
 
 class ExpenseTSVWrapper(object):
 
@@ -51,3 +56,49 @@ def UsersAvatarChangeTask(avatar, avatarman, user, home):
     db_session.remove()
 
     return os.path.join(home, url)
+
+
+@celery.task
+def CategoriesResetTask(user):
+    def key(*args):
+        return ''.join(args)
+
+    user_categories_counter = Counter()
+    user_categories = set()
+
+    expenses = (db_session.query(Expense, User).join(User)
+                    .filter_by(user_id=user.id)
+                    .filter(User.deleted == False)
+                    .order_by(Expense.created.asc()))
+    categories = []
+    for expense, user in expenses:
+        k = key(expense.user_id, expense.category)
+        if k not in user_categories:
+            color = config.CATEGORY_PALETTE[user_categories_counter[user.id]]
+
+            category = Category(user_id=expense.user_id,
+                                name=expense.category,
+                                foreground=color['foreground'],
+                                background=color['background'])
+            db_session.add()
+            try:
+                db_session.commit()
+                categories.append(category)
+            except sqlalchemy.exc.IntegrityError:
+                db_session.rollback()
+
+                category = (db_session.query(Category)
+                                .filter_by(user_id=user.id)
+                                .filter_by(name=expense.category)
+                                .first())
+                category.foreground = color['foreground']
+                category.background = color['background']
+
+                db_session.add(category)
+                db_session.commit()
+                categories.append(category)
+
+            user_categories.add(k)
+            user_categories_counter[expense.user_id] += 1
+    db_session.remove()
+    return categories
