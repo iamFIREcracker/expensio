1133309ff56d43d4d18a7f05ea1c4b8838bad7e2
Matteo Landi <matteo@matteolandi.net>
2013-03-16 19:01:09 +0100

    Remove override of effective logging level

--- app/logging.py
+++ app/logging.py
@@ -13,24 +13,14 @@ Implements the logging support for Flask.
 
 from __future__ import absolute_import
 
-from logging import getLogger, StreamHandler, Formatter, getLoggerClass, DEBUG
+from logging import getLogger, StreamHandler, Formatter, DEBUG
 
 
 def create_logger(config):
-    """Creates a logger for the given application. This logger works
-similar to a regular Python logger but changes the effective logging
-level based on the application's debug flag. Furthermore this
-function also removes all attached handlers in case there was a
-logger with the log name before.
-"""
-    Logger = getLoggerClass()
-
-    class DebugLogger(Logger):
-        def getEffectiveLevel(x):
-            if x.level == 0 and config.debug:
-                return DEBUG
-            return Logger.getEffectiveLevel(x)
-
+    """Creates a logger for the given application.  Furthermore this function
+    also removes all attached handlers in case there was a logger with the log
+    name before.
+    """
     class DebugHandler(StreamHandler):
         def emit(x, record):
             StreamHandler.emit(x, record) if config.debug else None
@@ -42,6 +32,5 @@ logger with the log name before.
     # just in case that was not a new logger, get rid of all the handlers
     # already attached to it.
     del logger.handlers[:]
-    logger.__class__ = DebugLogger
     logger.addHandler(handler)
     return logger
