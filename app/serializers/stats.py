4245cc6af7325dda885378e3e65afc8d019e1017
Matteo Landi <matteo@matteolandi.net>
2013-04-17 21:46:42 +0200

    Add serializers module with all the memory -> json converters

---
+++ app/serializers/stats.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from app import formatters
+
+
+class StatByCategorySerializer(object):
+    
+    def __init__(self, category, currency):
+        self.o = category
+        self.currency = currency
+
+    def to_dict(self):
+        return {
+            'name': self.o[0],
+            'updated': formatters.datetime(self.o[1]),
+            'income': formatters.amount(self.o[2]),
+            'outcome': formatters.amount(self.o[3]),
+            'currency': self.currency,
+        }
+
+
+class StatByDaySerializer(object):
+    
+    def __init__(self, day, currency):
+        self.o = day
+        self.currency = currency
+
+    def to_dict(self):
+        return {
+            'date': formatters.date(self.o.d['date']),
+            'income': formatters.amount(self.o.d['income']),
+            'outcome': formatters.amount(self.o.d['outcome']),
+            'currency': self.currency,
+        }
