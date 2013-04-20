f2933c58211f97db610e8c5da22c372ac61415eb
Matteo Landi <matteo@matteolandi.net>
2013-04-20 15:14:09 +0200

    Add app factory (should ease testing) and update launch scripts accordingly

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,6 @@ import os
 import web
 
 from app import config
-from app import models
-from app import sessions
 from app.database import db_session
 from app.logging import create_logger
 from .tools.app_processor import load_keyvalue
@@ -26,22 +24,27 @@ web.config.log_format = config.LOG_FORMAT
 web.config.debug = config.DEBUG
 web.config.debug_sql = config.DEBUG_SQL
 
-workingdir = os.getcwd()
-app = web.application(URLS, globals())
-dbpath = config.DATABASE_URL.replace('sqlite:///', '')
-db = web.database(dbn='sqlite', db=dbpath)
-session = web.session.Session(app, web.session.DBStore(db, 'session'))
 
-app.add_processor(web.loadhook(load_path_url))
-app.add_processor(web.loadhook(load_logger(create_logger(web.config))))
-app.add_processor(web.loadhook(load_render(workingdir)))
-app.add_processor(web.loadhook(load_session(session)))
-app.add_processor(web.loadhook(load_keyvalue('uploadman',
-                                             UploadManager(config.UPLOAD_DIR,
-                                                           workingdir))))
-app.add_processor(web.loadhook(load_keyvalue('avatarman',
-                                             UploadManager(config.AVATAR_DIR,
-                                                           workingdir))))
-app.add_processor(web.loadhook(load_keyvalue('exportman',
-                                             ExportManager(workingdir))))
-app.add_processor(load_sqla(db_session))
+def create_app():
+    """App factory."""
+    workingdir = os.getcwd()
+    app = web.application(URLS, globals())
+    dbpath = config.DATABASE_URL.replace('sqlite:///', '')
+    db = web.database(dbn='sqlite', db=dbpath)
+    session = web.session.Session(app, web.session.DBStore(db, 'session'))
+
+    app.add_processor(web.loadhook(load_path_url))
+    app.add_processor(web.loadhook(load_logger(create_logger(web.config))))
+    app.add_processor(web.loadhook(load_render(workingdir)))
+    app.add_processor(web.loadhook(load_session(session)))
+    app.add_processor(web.loadhook(load_keyvalue('uploadman',
+                                                UploadManager(config.UPLOAD_DIR,
+                                                            workingdir))))
+    app.add_processor(web.loadhook(load_keyvalue('avatarman',
+                                                UploadManager(config.AVATAR_DIR,
+                                                            workingdir))))
+    app.add_processor(web.loadhook(load_keyvalue('exportman',
+                                                ExportManager(workingdir))))
+    app.add_processor(load_sqla(db_session))
+
+    return app
