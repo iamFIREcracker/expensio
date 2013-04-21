bdf564fca3b61fc36e79341d5255cef7581ba901
Matteo Landi <matteo@matteolandi.net>
2013-04-21 19:52:30 +0200

    More index tests

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from tests.utils import url
 from tests.utils import login
 from tests.utils import TestCaseWithApp
 
 
-class TestApplication(TestCaseWithApp):
+class TestIndex(TestCaseWithApp):
 
     def test_anonymous_user_is_presented_with_the_info_page(self):
         resp = self.app.get('/')
@@ -26,3 +25,10 @@ class TestApplication(TestCaseWithApp):
                 'Income' in resp, 'Income widget missing')
         self.assertTrue(
                 'Outcome' in resp, 'Outcome widget missing')
+
+    def test_logged_user_can_decide_which_period_to_display(self):
+        login(self.app)
+        resp = self.app.get('/2012/12')
+        self.assertEquals('200 OK', resp.status)
+        self.assertTrue(
+                '2012, 12' in resp, 'Selected year-month title missing.')
