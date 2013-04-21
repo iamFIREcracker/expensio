1009aec49d0a2b0022920fbbb7d5fbafc29ac5e2
Matteo Landi <matteo@matteolandi.net>
2013-04-21 13:03:19 +0200

    Make setup.py use get_name and get_version app functions

--- setup.py
+++ setup.py
@@ -6,16 +6,17 @@ import os
 from setuptools import find_packages
 from setuptools import setup
 
+from app.utils import get_name
+from app.utils import get_version
 
-NAME = 'expenses'
-VERSION = '0.0.1'
+
 requirements = os.path.join(os.path.dirname(__file__), 'requirements.txt')
 INSTALL_REQUIRES = open(requirements).read().split()
 
 
 params = dict(
-    name=NAME,
-    version=VERSION,
+    name=get_name(),
+    version=get_version(),
     packages=find_packages(exclude=['fabfile']),
     install_requires=INSTALL_REQUIRES,
 )
