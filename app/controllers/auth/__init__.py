65c5e07e24d5ce200fccd1075b52e65efc84e91a
Matteo Landi <matteo@matteolandi.net>
2013-04-21 10:01:34 +0200

    Use relative imports for auth controllers

--- app/controllers/auth/__init__.py
+++ app/controllers/auth/__init__.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from google import LoginGoogleHandler
-from google import LoginGoogleAuthorizedHandler
-from facebook import LoginFacebookHandler
-from facebook import LoginFacebookAuthorizedHandler
-from twitter import LoginTwitterHandler
-from twitter import LoginTwitterAuthorizedHandler
-from fake import LoginFakeHandler
-from fake import LoginFakeAuthorizedHandler
+from .google import LoginGoogleHandler
+from .google import LoginGoogleAuthorizedHandler
+from .facebook import LoginFacebookHandler
+from .facebook import LoginFacebookAuthorizedHandler
+from .twitter import LoginTwitterHandler
+from .twitter import LoginTwitterAuthorizedHandler
+from .fake import LoginFakeHandler
+from .fake import LoginFakeAuthorizedHandler
 
 __all__ = [LoginGoogleHandler, LoginGoogleAuthorizedHandler,
         LoginFacebookHandler, LoginFacebookAuthorizedHandler,
