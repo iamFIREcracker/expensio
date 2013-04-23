90e2b3d4cd2459d8ebbbe07c01958b22ab176afe
Matteo Landi <matteo@matteolandi.net>
2013-04-23 07:38:09 +0200

    Add documentation + tests to api decorator.

--- app/tools/request_decorators.py
+++ app/tools/request_decorators.py
@@ -7,17 +7,44 @@ import web
 
 def api(func):
     """Checks that the current request has the header ``HTTP_ACCEPT`` set, and
-    that the specified value is actually supported.
+    that the specified value is actually supported by the server.
 
-    At the moment, the sole content-type supported is ``json``.
-    
-    If an unsupported content-type is passed then the current request will
-    return '406 Not acceptable'."""
-    def inner(self, *args, **kwargs):
+    If an unsupported content-type is passed a ``web.notacceptable`` exception
+    is raised and the status '406 Not acceptable' is sent to the client.
+
+
+    >>> class MyNotAcceptable(Exception):
+    ...   pass
+    >>> web.notacceptable = MyNotAcceptable
+    >>> request = lambda: 'Hello world'
+
+
+    >>> web.ctx['environ'] = dict()
+    >>> api(request)()
+    Traceback (most recent call last):
+        ...
+    MyNotAcceptable
+
+
+    >>> web.ctx['environ'] = dict(HTTP_ACCEPT='application/xml')
+    >>> api(request)()
+    Traceback (most recent call last):
+        ...
+    MyNotAcceptable
+
+    >>> web.ctx['environ'] = dict(HTTP_ACCEPT='application/json')
+    >>> api(request)()
+    'Hello world'
+    """
+    def inner(*args, **kwargs):
         accept = web.ctx.environ.get('HTTP_ACCEPT', '').split(',')
         if 'application/json' not in accept:
             raise web.notacceptable()
-        return func(self, *args, **kwargs)
+        return func(*args, **kwargs)
     return inner
 
 
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
