2b1af83b3b5809a78a759825486c0e3696162905
Matteo Landi <matteo@matteolandi.net>
2013-04-24 07:50:51 +0200

    Add documentation usersAvatarChange controller + change API.

--- app/controllers/users.py
+++ app/controllers/users.py
@@ -9,6 +9,8 @@ from app.forms import users_avatar
 from app.forms import users_edit
 from app.serializers import UserSerializer
 from app.upload import UploadedFile
+from app.tools.request_decorators import api
+from app.utils import accepted
 from app.utils import jsonify
 from app.utils import logout
 from app.utils import me
@@ -19,9 +21,37 @@ from app.utils import BaseHandler
 
 class UsersAvatarChange(BaseHandler):
 
+    @api
     @protected
     @me
     def POST(self, id):
+        """Changes the avatar of the user identified by ``id``.
+
+        The 'HTTP_ACCEPT' header is required to allow the controller to specify
+        the acceptable media type for the response.
+
+        There should be a logged-in user behind this request.
+
+        The specified ``id`` should match the one of the logged-in user.
+
+        If all these prerequisites hold true then the controller will check for 
+        the existence of the field ``avatar`` containing an uploaded file.
+
+        On success the controller will spawn an asynchronous task (in charge of
+        processing the image) and will return '202 Accepted'.  Note that the
+        'Location' header will be filled with the URI handy to check the status
+        of the asynchronous task.
+
+        On error an object with error descriptions and of the specified media
+        format is returned to the caller:
+        {
+            "success": false,
+            "errors":
+            [
+                "avatar": "Missing"
+            ]
+        }
+        """
         avatar = UploadedFile('avatar')
         form = users_avatar()
 
@@ -31,10 +61,14 @@ class UsersAvatarChange(BaseHandler):
                         if i.note is not None))
         else:
             task_id = tasks.UsersAvatarChangeTask.delay(
-                    avatar, web.ctx.avatarman, self.current_user(), web.ctx.home).task_id
-            return jsonify(success=True,
-                    goto='/users/%s/avatar/change/status/%s' % (
-                            self.current_user().id, task_id))
+                    avatar, web.ctx.avatarman, self.current_user(),
+                    web.ctx.home).task_id
+            web.header(
+                    'Location',
+                    '/users/%(user_id)s/avatar/change/status/%(task_id)s' % dict(
+                        user_id=self.current_user().id, task_id=task_id
+                    ))
+            raise web.accepted()
 
 
 class UsersAvatarChangeStatusHandler(BaseHandler):
