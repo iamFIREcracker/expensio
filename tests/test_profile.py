1e97ba815bfbef7b19c5a25fd8e55ea49343d406
Matteo Landi <matteo@matteolandi.net>
2013-04-25 16:25:23 +0200

    More avatar-profile tests

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from tests.utils import login
+import webtest
+
+from tests.utils import register
+from tests.utils import upload
+from tests.utils import url
 from tests.utils import TestCaseWithApp
 
 
@@ -10,9 +14,10 @@ class TestProfile(TestCaseWithApp):
     def test_anonymous_user_cannot_access_the_profile_page(self):
         resp = self.app.get('/profile')
         self.assertEquals('302 Found', resp.status)
+        self.assertEquals(url('/'), resp.location)
 
     def test_logged_user_can_view_the_profile_page(self):
-        login(self.app)
+        register(self.app)
         resp = self.app.get('/profile')
         self.assertEquals('200 OK', resp.status)
         self.assertIn('Avatar', resp)
@@ -22,3 +27,50 @@ class TestProfile(TestCaseWithApp):
         self.assertIn('Facebook', resp)
         self.assertIn('Twitter', resp)
         self.assertIn('Fake', resp)
+
+    def test_http_accept_header_is_required_to_change_avatar(self):
+        with self.assertRaises(webtest.AppError) as cm:
+            self.app.post('/v1/users/invalid-uuid/avatar/change')
+            self.assertEqual(
+                    "Bad response: 406 Not Acceptable", cm.exception)
+
+    def test_logged_user_cannot_change_avatar_of_another_user(self):
+        register(self.app)
+        with self.assertRaises(webtest.AppError) as cm:
+            self.app.post(
+                    '/v1/users/invalid-uuid/avatar/change', extra_environ=dict(
+                        HTTP_ACCEPT='application/json'
+                    ))
+            self.assertEqual(
+                    "Bad response: 401 Unauthorized", cm.exception)
+
+    def test_logged_user_cannot_change_avatar_without_specifying_it(self):
+        user_id = register(self.app)
+        resp = self.app.post(
+                '/v1/users/%(user_id)s/avatar/change' % dict(user_id=user_id),
+                extra_environ=dict(HTTP_ACCEPT='application/json'))
+        self.assertFalse(
+                resp.json['success'], 'An error should have been received')
+
+    def test_logged_user_can_change_avatar(self):
+        user_id = register(self.app)
+        resp = self.app.post(
+                '/v1/users/%(user_id)s/avatar/change' % dict(user_id=user_id),
+                dict(avatar=upload('tests/avatar.png')), extra_environ=dict(
+                    HTTP_ACCEPT='application/json'
+                ))
+        self.assertEquals('202 Accepted', resp.status)
+        self.assertTrue(
+            resp.location.startswith(
+                    '/v1/users/%(user_id)s/avatar/change/' % dict(
+                        user_id=user_id
+                    )))
+
+        resp = self.app.get(resp.location, extra_environ=dict(
+            HTTP_ACCEPT='application/json'
+        ))
+
+        self.assertEquals('303 See Other', resp.status)
+        self.assertTrue(
+                resp.location.startswith(url('/static/avatars/')),
+                'The redirect should point to a static resource')
