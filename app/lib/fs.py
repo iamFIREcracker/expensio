fa870c1c8ef3ab23ad7019c9e0d2e165db2d0c74
Matteo Landi <matteo@matteolandi.net>
2013-05-19 17:27:54 +0200

    Make fsadapter.rename manage multiple renaming operations

--- app/lib/fs.py
+++ app/lib/fs.py
@@ -12,17 +12,23 @@ class FileSystemAdapter(Publisher):
         """Creates a temporary file with extension ``ext`` and dump ``file``
         into it.
 
-        On success, the writer will emit a 'tempfile_created' message followed
-        by the name of temporary file created.
+        On success the method will emit a 'tempfile_created' message followed by
+        the name of temporary file created.
         """
         suffix = '.' + ext
         with tempfile.NamedTemporaryFile("w+b", suffix=suffix, delete=False) as tmp:
             tmp.write(file.read())
             self.publish('tempfile_created', tmp.name)
 
-    def rename(self, source, destination):
-        """Rename ``source`` into ``destination``.
+    def rename(self, *mappings):
+        """Applies all the _renaming_ transformations defined into ``mappings``.
 
-        If ``destination`` is a directory, this method will throw an
-        ``OSError``."""
-        os.rename(source, destination)
+        ``mappings`` is a collection of tuples, each defining a rename
+        operation:  tuples are made by two elements, the first containing the
+        file to be renamed and the second containing the destination file.
+
+        On success the method will emit a 'files_renamed' message containing the
+        the collection of _renaming_ transformations (i.e. ``mappings``).
+        """
+        map(lambda s, d: os.rename(s, d), mappings)
+        self.publish('files_renamed', *mappings)
