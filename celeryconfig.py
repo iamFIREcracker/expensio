5000000d153651587a6f8bbb03d1bda83a8d8929
Matteo Landi <matteo@matteolandi.net>
2013-05-27 08:05:19 +0200

    Move celery configuration out of the app package.

---
+++ celeryconfig.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+BROKER_URL = 'amqp://expenses:expenses@localhost:5672'
+
+CELERY_IMPORTS = ('app.tasks',)
+
+CELERY_RESULT_BACKEND = 'amqp'
+CELERY_RESULT_DBURI = 'amqp://expenses:expenses@localhost:5672'
