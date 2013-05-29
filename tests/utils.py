352cde7f5578d4d3d4fc8e24bb85b628becf1572
Matteo Landi <matteo@matteolandi.net>
2013-05-29 07:49:09 +0200

    Disable custom test setup:  everything needed is read from local configs

--- tests/utils.py
+++ tests/utils.py
@@ -76,42 +76,15 @@ class TestCaseWithApp(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.dbfile = temp_file()
-
-        # Configures the database
-        # XXX Cannot move this below the import of ``init_db`` 
-        web.config.db = 'sqlite:///' + cls.dbfile
-
         # Initialize the database
         from app.database import init_db
         init_db()
 
-        # Configure celery
-        #from app.celery import celery
-
-        #celery.conf.update(
-            ##BROKER_BACKEND='memory',
-            #CELERY_ALWAYS_EAGER=True,
-            #CELERY_EAGER_PROPAGATE_EXCEPTIONS=True
-        #)
-
         # Create the application
         from app import create_app
 
-        # Disable custom logging
-        web.config.log_enable = False
-        # Disable sql logging, otherwise webtest will consider them as errors
-        web.config.debug_sql = False
-        # Disable debug mode
-        web.config.debug = False
-
         middleware = []
         cls.app = webtest.TestApp(create_app().wsgifunc(*middleware))
-
-    @classmethod
-    def tearDownClass(cls):
-        os.unlink(cls.dbfile)
-
 
     def setUp(self):
         self.app.reset()
